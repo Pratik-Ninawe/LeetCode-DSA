2051db642f7b9d8d02ab52f4812bdcd8b214f78b
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-05-24 01:20:58 +0530

    Time: 111 ms (51.3%), Space: 224.1 MB (86.24%) - LeetHub

--- 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
+++ 3647-zero-array-transformation-iii/3647-zero-array-transformation-iii.cpp
@@ -1,25 +1,45 @@
-class Solution {
+class Solution
+{
 public:
-    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
-        sort(queries.begin(), queries.end());
-        priority_queue<int> available;
-        priority_queue<int, vector<int>, greater<int>> assigned;
-        int count = 0;
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries)
+    {
+        int n = nums.size();
+        int m = queries.size();
 
-        for (int time = 0, k = 0; time < nums.size(); time++) {
-            while (!assigned.empty() && assigned.top() < time)
-                assigned.pop();
-            while (k < queries.size() && queries[k][0] <= time) 
-                available.push(queries[k++][1]);
-            while (assigned.size() < nums[time] && 
-                  !available.empty() && available.top() >= time) {
-                assigned.push(available.top());
-                available.pop();
-                count++;
+        vector<int> workload(n + 1, 0);  // Difference array
+        sort(queries.begin(), queries.end());
+
+        priority_queue<int> available;
+        int qIndex = 0;
+
+        for (int time = 0; time < n; ++time)
+        {
+            if (time > 0)
+            {
+                workload[time] += workload[time - 1];
             }
-            if (assigned.size() < nums[time])
-                return -1;
+
+            // Load queries that start at this time
+            while (qIndex < m && queries[qIndex][0] == time)
+            {
+                available.push(queries[qIndex][1]);
+                ++qIndex;
+            }
+
+            // Assign queries until we meet nums[time]
+            while (workload[time] < nums[time])
+            {
+                if (available.empty() || available.top() < time)
+                {
+                    return -1;
+                }
+
+                workload[time]++;
+                workload[available.top() + 1]--;
+                available.pop();
+            }
         }
-        return queries.size() - count;
+
+        return available.size(); // Remaining unused queries
     }
 };
