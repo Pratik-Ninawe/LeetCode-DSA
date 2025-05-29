cb4433175eb073e8b150f28b2a9a60ebeb4852bf
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-05-30 00:33:28 +0530

    Time: 277 ms (93.88%), Space: 341.7 MB (58.5%) - LeetHub

--- 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
+++ 3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii/3645-maximize-the-number-of-target-nodes-after-connecting-trees-ii.cpp
@@ -1,37 +1,40 @@
 class Solution {
 public:
-    vector<vector<int>> buildList(const vector<vector<int>>& edges) {
-        vector<vector<int>> adj(edges.size() + 1);
-        for (auto &e : edges) {
-            adj[e[0]].push_back(e[1]);
-            adj[e[1]].push_back(e[0]);
+    int dfs(int node, int parent, int depth,
+            const vector<vector<int>>& children, vector<int>& color) {
+        int res = 1 - depth % 2;
+        color[node] = depth % 2;
+        for (int child : children[node]) {
+            if (child == parent) {
+                continue;
+            }
+            res += dfs(child, node, depth + 1, children, color);
         }
-        return adj;
+        return res;
     }
 
-    void dfsColor(const vector<vector<int>>& adj, int u, int parent,
-                  vector<int>& color, int& evenCnt, int& oddCnt) {
-        if (color[u] == 0) evenCnt++;
-        else oddCnt++;
-        for (int v : adj[u]) if (v != parent) {
-            color[v] = color[u] ^ 1;
-            dfsColor(adj, v, u, color, evenCnt, oddCnt);
+    vector<int> build(const vector<vector<int>>& edges, vector<int>& color) {
+        int n = edges.size() + 1;
+        vector<vector<int>> children(n);
+        for (const auto& edge : edges) {
+            children[edge[0]].push_back(edge[1]);
+            children[edge[1]].push_back(edge[0]);
         }
+        int res = dfs(0, -1, 0, children, color);
+        return {res, n - res};
     }
 
-    vector<int> maxTargetNodes(vector<vector<int>>& edges1, vector<vector<int>>& edges2) {
-        auto adjA = buildList(edges1), adjB = buildList(edges2);
-        int n = adjA.size(), m = adjB.size();
-        vector<int> colorA(n, -1), colorB(m, -1);
-        int evenA = 0, oddA = 0, evenB = 0, oddB = 0;
-        colorA[0] = 0;
-        dfsColor(adjA, 0, -1, colorA, evenA, oddA);
-        colorB[0] = 0;
-        dfsColor(adjB, 0, -1, colorB, evenB, oddB);
-        int maxiB = max(evenB, oddB);
-        vector<int> res(n);
-        for (int i = 0; i < n; i++)
-            res[i] = (colorA[i] == 0 ? evenA : oddA) + maxiB;
+    vector<int> maxTargetNodes(vector<vector<int>>& edges1,
+                               vector<vector<int>>& edges2) {
+        int n = edges1.size() + 1, m = edges2.size() + 1;
+        vector<int> color1(n);
+        vector<int> color2(m);
+        vector<int> count1 = build(edges1, color1);
+        vector<int> count2 = build(edges2, color2);
+        vector<int> res(edges1.size() + 1);
+        for (int i = 0; i < n; i++) {
+            res[i] = count1[color1[i]] + max(count2[0], count2[1]);
+        }
         return res;
     }
 };
