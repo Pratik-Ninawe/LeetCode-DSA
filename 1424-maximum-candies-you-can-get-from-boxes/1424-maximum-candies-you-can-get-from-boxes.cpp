8c5bdabec70398d3a00c905fb3db1b154c5337ec
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-06-04 00:48:15 +0530

    Time: 0 ms (100%), Space: 44.4 MB (50.48%) - LeetHub

--- 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
+++ 1424-maximum-candies-you-can-get-from-boxes/1424-maximum-candies-you-can-get-from-boxes.cpp
@@ -1,40 +1,23 @@
 class Solution {
 public:
-    int maxCandies(vector<int>& stat, vector<int>& choco, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& curBoxes) {
-        deque<int> dq;
-
-        // process / put current box in queue
-        for(int i=0;i<curBoxes.size();i++){
-            int box_ind = curBoxes[i];
-
-            if(stat[box_ind] == 0){dq.push_back(box_ind);}
-            else{dq.push_front(box_ind);}
-        }
-
-        int ans = 0;
-
-        // start processing each box
-        while(!dq.empty()){
-            int box = dq.front();
-            dq.pop_front();
-
-            if(stat[box]==0){
-                // continue;  // use it if you want to be sure to look at all locked box 
-                break;
-            }
-            else if(stat[box]==1){
-                ans += choco[box];
-                for(auto& key:keys[box]){
-                    stat[key] = 1;
+    int maxCandies(vector<int>& vis, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        bool foundOpenable = true;
+        int totalCandies = 0;
+        while (!initialBoxes.empty() && foundOpenable) {
+            foundOpenable = false;
+            vector<int> nextBoxes;
+            for (int boxId : initialBoxes) {
+                if (vis[boxId]) {
+                    foundOpenable = true;
+                    nextBoxes.insert(end(nextBoxes), begin(containedBoxes[boxId]), end(containedBoxes[boxId]));
+                    for (int keyId : keys[boxId]) vis[keyId] = 1;
+                    totalCandies += candies[boxId];
+                } else {
+                    nextBoxes.push_back(boxId);
                 }
             }
-
-            for(auto& new_box_ind : containedBoxes[box]){
-                if(stat[new_box_ind]==0){dq.push_back(new_box_ind);}
-                else{dq.push_front(new_box_ind);}
-            }
+            swap(initialBoxes, nextBoxes);
         }
-
-        return ans;
+        return totalCandies;
     }
 };
