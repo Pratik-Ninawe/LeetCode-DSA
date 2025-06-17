492b5e1afc270f78a90c1c163fa2f3af7dc8644b
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-06-18 00:41:02 +0530

    Time: 2 ms (90.91%), Space: 10 MB (78.41%) - LeetHub

--- 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
+++ 3682-count-the-number-of-arrays-with-k-matching-adjacent-elements/3682-count-the-number-of-arrays-with-k-matching-adjacent-elements.cpp
@@ -1,29 +1,43 @@
-class Solution {
-public:
-    const int MOD = 1e9 + 7;
+const int MOD = 1e9 + 7;
+const int MX = 1e5;
 
-    long long modPow(long long a, long long b) {
+long long fact[MX];
+long long inv_fact[MX];
+
+class Solution {
+    long long qpow(long long x, int n) {
         long long res = 1;
-        while (b > 0) {
-            if (b % 2 == 1)
-                res = res * a % MOD;
-            a = a * a % MOD;
-            b /= 2;
+        while (n) {
+            if (n & 1) {
+                res = res * x % MOD;
+            }
+            x = x * x % MOD;
+            n >>= 1;
         }
         return res;
     }
 
-    long long modInv(long long x) {
-        return modPow(x, MOD - 2);
+    long long comb(int n, int m) {
+        return fact[n] * inv_fact[m] % MOD * inv_fact[n - m] % MOD;
+    }
+    void init() {
+        if (fact[0]) {
+            return;
+        }
+        fact[0] = 1;
+        for (int i = 1; i < MX; i++) {
+            fact[i] = fact[i - 1] * i % MOD;
+        }
+
+        inv_fact[MX - 1] = qpow(fact[MX - 1], MOD - 2);
+        for (int i = MX - 1; i; i--) {
+            inv_fact[i - 1] = inv_fact[i] * i % MOD;
+        }
     }
 
+public:
     int countGoodArrays(int n, int m, int k) {
-        vector<long long> fact(n, 1);
-        for (int i = 1; i < n; ++i)
-            fact[i] = fact[i - 1] * i % MOD;
-
-        long long comb = fact[n - 1] * modInv(fact[k]) % MOD * modInv(fact[n - 1 - k]) % MOD;
-        long long res = comb * m % MOD * modPow(m - 1, n - 1 - k) % MOD;
-        return res;
+        init();
+        return comb(n - 1, k) * m % MOD * qpow(m - 1, n - k - 1) % MOD;
     }
 };
