a95318cd52c045a50b88b2a195df355bc54e450b
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-06-21 00:30:44 +0530

    Time: 37 ms (96.67%), Space: 38 MB (90.61%) - LeetHub

--- 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
+++ 3754-maximum-manhattan-distance-after-k-changes/3754-maximum-manhattan-distance-after-k-changes.cpp
@@ -1,34 +1,25 @@
 class Solution {
 public:
     int maxDistance(string s, int k) {
-        int ans = 0;
-        int north = 0, south = 0, east = 0, west = 0;
-        for (char it : s) {
-            switch (it) {
+        int latitude = 0, longitude = 0, ans = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            switch (s[i]) {
                 case 'N':
-                    north++;
+                    latitude++;
                     break;
                 case 'S':
-                    south++;
+                    latitude--;
                     break;
                 case 'E':
-                    east++;
+                    longitude++;
                     break;
                 case 'W':
-                    west++;
+                    longitude--;
                     break;
             }
-            int times1 =
-                min({north, south, k});  // modification times for N and S
-            int times2 = min(
-                {east, west, k - times1});  // modification times for E and W
-            ans = max(ans,
-                      count(north, south, times1) + count(east, west, times2));
+            ans = max(ans, min(abs(latitude) + abs(longitude) + k * 2, i + 1));
         }
         return ans;
     }
-
-    int count(int drt1, int drt2, int times) {
-        return abs(drt1 - drt2) + times * 2;
-    }  // Calculate modified Manhattan distance
 };
