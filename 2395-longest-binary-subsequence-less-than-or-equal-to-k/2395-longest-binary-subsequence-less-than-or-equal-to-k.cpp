bdc79d00ff5f63f41000d37a8c5a5e74ca5597ba
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-06-27 00:21:55 +0530

    Time: 0 ms (100%), Space: 9.4 MB (96.83%) - LeetHub

--- 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
+++ 2395-longest-binary-subsequence-less-than-or-equal-to-k/2395-longest-binary-subsequence-less-than-or-equal-to-k.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     int longestSubsequence(string s, int k) {
-        int sm = 0;
-        int cnt = 0;
-        int bits = 32 - __builtin_clz(k);
-        for (int i = 0; i < s.size(); ++i) {
-            char ch = s[s.size() - 1 - i];
-            if (ch == '1') {
-                if (i < bits && sm + (1 << i) <= k) {
-                    sm += 1 << i;
-                    cnt++;
-                }
-            } else {
-                cnt++;
-            }
+        int n = s.size();
+        int zeros = 0, ones = 0;
+        long long value = 0, pow = 1;
+
+        for (char c : s) {
+            if (c == '0') zeros++;
         }
-        return cnt;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (s[i] == '1') {
+                if (value + pow > k) {
+                    continue; // Skip this '1' as it would exceed k
+                }
+                value += pow;
+                ones++;
+            }
+            pow <<= 1;
+            if (pow > k) break; // Further bits would exceed k
+        }
+
+        return zeros + ones;
     }
 };
