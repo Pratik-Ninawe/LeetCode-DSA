5fad7d8dd6e8a0d57c47c1aa28f449bb4319347c
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-07-22 22:01:32 +0530

    Time: 0 ms (100%), Space: 8.6 MB (59.61%) - LeetHub

---
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxRepeating(string sequence, string word) {
+        
+        int cnt=0;
+        string s=word;
+        while(sequence.find(s) != string::npos){
+            cnt++;
+            s += word;    
+        }
+        return cnt;
+    }
+};
