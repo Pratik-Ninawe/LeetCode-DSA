ba59a29beb66f0cf41ac277ccd9de3c7deb02e3b
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-08-06 23:57:51 +0530

    Time: 31 ms (98.61%), Space: 184.1 MB (86.35%) - LeetHub

--- 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
+++ 3791-fruits-into-baskets-iii/3791-fruits-into-baskets-iii.cpp
@@ -1,62 +1,38 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        vector<int> sect_mx;
-        int m = baskets.size();
-        int a = sqrt(m); // size of one array
+        int n = baskets.size();
+        int N = 1;
+        while (N <= n) N <<= 1;
 
-        int cnt=0, mx=0;
-        for(int i=0;i<m;i++){
-            if(cnt==a){
-                // creating sector of size a
-                sect_mx.push_back(mx);
-                mx=baskets[i], cnt=1; 
+        vector<int> segTree(N << 1);
+        for (int i = 0; i < n; ++i)
+            segTree[N + i] = baskets[i];
+        
+        for (int i = N - 1; i > 0; --i)
+            segTree[i] = max(segTree[2 * i], segTree[2 * i + 1]);
+
+        int count = 0;
+        for (int fruit : fruits) {
+            int index = 1;
+            if (segTree[index] < fruit) {
+                count++;
+                continue;
             }
-            else{
-                cnt++;
-                mx = max(mx, baskets[i]);
+
+            while (index < N) {
+                if (segTree[2 * index] >= fruit)
+                    index = 2 * index;
+                else
+                    index = 2 * index + 1;
+            }
+
+            segTree[index] = -1;
+            while (index > 1) {
+                index >>= 1;
+                segTree[index] = max(segTree[2 * index], segTree[2 * index + 1]);
             }
         }
-
-        // last sector
-        sect_mx.push_back(mx);
-
-        int remain = 0;
-
-        // start allocating
-        for(int i=0;i<fruits.size();i++){
-            
-            int k = 0, set = 1;
-            for(int j=0;j<m;j+=a){
-                if(sect_mx[k] < fruits[i]){
-                    // skip this segment
-                    k++;
-                    continue;
-                }
-
-                // find place to allocate
-                for(int r=j;r<min(j+a, m);r++){
-                    if(baskets[r] >= fruits[i]){
-                        set = 0;
-                        baskets[r] = 0;
-                        break;
-                    }
-                }
-
-                // if fruit is allocated in a sector
-                if(set==0){
-                    sect_mx[k] = 0; // find new mx
-                    // update new sector mx
-                    for(int r=j;r<min(j+a, m);r++){
-                        sect_mx[k] = max(baskets[r], sect_mx[k]);
-                    }
-                    break;
-                }
-            }
-
-            remain += set;
-        }
-
-        return remain;
+        return count;
     }
 };
