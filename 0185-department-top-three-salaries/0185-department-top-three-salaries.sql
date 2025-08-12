a3397ede6cb443c4b79e05510c79fefa8acea8a5
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-08-13 00:13:22 +0530

    Time: 1112 ms (35.28%), Space: 0B (100%) - LeetHub

--- 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-select department ,employee ,salary from (select d.name as department ,
-    e.name as employee , e.salary, dense_rank () over (partition by e.departmentid order by salary desc) as rnk from employee e join department d on e.departmentid = d.id)
-    as ranked where rnk<4;
+select department ,employee ,salary from (select d.name as department ,e.name as employee , e.salary, dense_rank () over (partition by e.departmentid order by salary desc) as rnk 
+from employee e join department d on e.departmentid = d.id) as ranked 
+where rnk<4;
