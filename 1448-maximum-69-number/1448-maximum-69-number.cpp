f680945158b9ac6fbc98438d393bd035531b5aae
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-08-17 18:19:25 +0530

    Time: 0 ms (100%), Space: 8.1 MB (41.8%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.cpp
+++ 1448-maximum-69-number/1448-maximum-69-number.cpp
@@ -1,12 +1,13 @@
 class Solution {
 public:
     int maximum69Number (int num) {
-        constexpr int three10[5]={0, 3, 30, 300, 3000};
-        int a=-1;
-        for(int n=num, d=0; n>0; n/=10, d++) {
-            int r=n%10;
-            if (r==6) a=d;
+        string s = to_string(num);
+        for (int i = 0; i < s.length(); i++) {
+            if (s[i] == '6') {
+                s[i] = '9'; // Change the first '6' to '9'
+                break;      // Only one change allowed
+            }
         }
-        return num+three10[a+1];     
+        return stoi(s);     
     }
 };
