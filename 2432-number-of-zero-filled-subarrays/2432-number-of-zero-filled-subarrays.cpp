fc29dbf88c7e741d5d5f851218fdb1717daeeff0
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-08-19 16:34:55 +0530

    Time: 1 ms (50.61%), Space: 111.2 MB (95.09%) - LeetHub

--- 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
+++ 2432-number-of-zero-filled-subarrays/2432-number-of-zero-filled-subarrays.cpp
@@ -12,12 +12,5 @@ public:
             }
         }
         return cnt;
-        // int a=0;
-        // for(int i=0;i<nums.size();i++){
-        //     if(nums[i] == 0){
-        //         a++;
-        //     }
-
-        // }
     }
 };
