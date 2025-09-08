687943fd35bd3cb18e4e96e089f8fb50283f5fc5
Pratik-Ninawe <114810053+Pratik-Ninawe@users.noreply.github.com>
2025-09-08 14:25:18 +0530

    Time: 0 ms (100%), Space: 9.7 MB (5.14%) - LeetHub

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/1440-convert-integer-to-the-sum-of-two-no-zero-integers.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
     vector<int> getNoZeroIntegers(int n) {
-        auto check = [](int x) {
-            while (x > 0) {
-                if (x % 10 == 0) return false;
-                x /= 10;
-            }
-            return true;
-        };
-        for (int i = 1; i < n; ++i) {
-            int j = n - i;
-            if (check(i) && check(j)) {
-                return {i, j};
+        for (int i = 1; i < n; i++) {
+            int left = i;
+            int right = n - i;
+            if (!containsZero(left) && !containsZero(right)) {
+                return {left, right};
             }
         }
         return {};
+    }
+
+    bool containsZero(int n) {
+        string str = to_string(n);
+        return str.find('0') != string::npos;
     }
 };
